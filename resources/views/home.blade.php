a8d9c793189e66ccd7ae61faf40a87e75fa7a971
Naoki Tsuchiya <n-tsuchiya@sankyofrontier.com>
2019-02-20 13:14:41 +0900

    Add post field.

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -2,7 +2,7 @@
 
 @section('content')
 <div class="container">
-    <div class="row justify-content-center">
+    <div class="row justify-content-center p-3">
         <div class="col-md-8">
             @if (session('status'))
                 <div class="alert alert-success" role="alert">
@@ -12,5 +12,23 @@
                 <a class="btn btn-primary" href="{{ route('post') }}" role="button">post</a>
         </div>
     </div>
+
+    <div class="p-3">
+        <h3>Your post</h3>
+        @forelse($posts as $post)
+            <div class="card">
+                <div class="card-body">
+                    <h4 class="card-title">{{ $post->title }}</h4>
+                    <div class="card-subtitle">
+                        <small class="text-right font-weight-light">posted at {{ $post->created_at }}</small>
+                    </div>
+                    <p class="card-text">{{ $post->content }}</p>
+                </div>
+            </div>
+        @empty
+            <p>There is no your post.</p>
+        @endforelse
+    </div>
+
 </div>
 @endsection
