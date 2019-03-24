b7723e12e82ef6419cee85ede1f360575ab2a727
Naoki Tsuchiya <ntsuchiya1105@gmail.com>
2019-03-24 12:56:41 +0900

    Add pagination to home

--- app/Http/Controllers/HomeController.php
+++ app/Http/Controllers/HomeController.php
@@ -6,6 +6,9 @@ use Illuminate\Http\Request;
 
 class HomeController extends Controller
 {
+
+    private const PAGINATE_SIZE = 10;
+
     /**
      * Create a new controller instance.
      *
@@ -20,11 +23,11 @@ class HomeController extends Controller
      * Show the application dashboard.
      *
      * @param Request $request
-     * @return \Illuminate\Contracts\Support\Renderable
+     * @return \Illuminate\Contracts\View\Factory|\Illuminate\View\View
      */
     public function index(Request $request)
     {
-        $posts = $request->user()->posts;
+        $posts = $request->user()->posts()->orderBy('created_at', 'desc')->simplePaginate(static::PAGINATE_SIZE);
         return view('home', compact('posts'));
     }
 }
--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -7,25 +7,27 @@
             {{ session('status') }}
         </div>
     @endif
-    <div class="list-group">
-        <h3>Your posts</h3>
-        @forelse($posts as $post)
-            <div class="list-group-item">
-                <div class="list-item-header d-flex align-items-end">
-                    <h4 class="mb-0 mr-5 list-item-title">
-                        <a href="{{ url('/'. $post->user_id . '/posts/' . $post->id) }}">
-                            {{ $post->title }}
-                        </a>
-                    </h4>
-                    <div class="list-item-dateTime">
-                        posted at {{ $post->created_at }}
+    <h3>Your posts</h3>
+    @if(count($posts) === 0)
+        <p>There is no your post.</p>
+    @else
+        <div class="list-group">
+            @foreach($posts as $post)
+                <div class="list-group-item">
+                    <div class="list-item-header d-flex align-items-end">
+                        <h4 class="mb-0 mr-5 list-item-title">
+                            <a href="{{ url('/'. $post->user_id . '/posts/' . $post->id) }}">
+                                {{ $post->title }}
+                            </a>
+                        </h4>
+                        <div class="list-item-dateTime">
+                            posted at {{ $post->created_at }}
+                        </div>
                     </div>
                 </div>
-            </div>
-        @empty
-            <p>There is no your post.</p>
-        @endforelse
-    </div>
-
+            @endforeach
+        </div>
+        {{ $posts->links() }}
+    @endif
 </div>
 @endsection
